1afc909f9ff732f47e5f7562ee5a1614f8c1a633
anonymous1643 <j71498520@gmail.com>
2025-07-13 14:32:12 -0700

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command fails
+echo "Running training script..."
+python3 Code/ForwardForecastModel.py
+
+echo "Running evaluation script..."
+python3 Code/EvaluateFFM.py
 
